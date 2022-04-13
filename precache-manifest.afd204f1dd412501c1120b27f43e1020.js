4285bc77464d38b279ef6806e4da1579c3a7cf31
Vlad-Shatskiy <serrbian@gmail.com>
2022-04-13 14:14:16 +0500

    Updates

---
+++ precache-manifest.afd204f1dd412501c1120b27f43e1020.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d2c82df4240c34b7be070a41ab9781e",
+    "url": "/keeper-app/index.html"
+  },
+  {
+    "revision": "66bda0722348e7a42142",
+    "url": "/keeper-app/static/js/2.953e52f2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/keeper-app/static/js/2.953e52f2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "84c68ae34ac22a651c10",
+    "url": "/keeper-app/static/js/main.ab96a6f1.chunk.js"
+  },
+  {
+    "revision": "043c4bc9b45c8772bfac",
+    "url": "/keeper-app/static/js/runtime-main.8929710f.js"
+  }
+]);
